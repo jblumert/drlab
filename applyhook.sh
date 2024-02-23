11c7158bc049a3439c5bfefc09513be60695113a
jblumert <66739278+jblumert@users.noreply.github.com>
2024-02-23 09:17:31 -0500

    Update applyhook.sh
    updated script with echo

--- applyhook.sh
+++ applyhook.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #oc get policyassignment  -n ibm-spectrum-fusion-ns
 echo "oc -n ibm-spectrum-fusion-ns patch policyassignment $1 --type merge -p '{"spec":{"recipe":{"name":"hook-recipe00", "namespace":"ibm-spectrum-fusion-ns", "apiVersion":"spp-data-protection.isf.ibm.com/v1alpha1"}}}'"
+oc -n ibm-spectrum-fusion-ns patch policyassignment $1 --type merge -p '{"spec":{"recipe":{"name":"hook-recipe00", "namespace":"ibm-spectrum-fusion-ns", "apiVersion":"spp-data-protection.isf.ibm.com/v1alpha1"}}}'
